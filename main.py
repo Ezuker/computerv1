10cb1c45047d7ee403af66bec475cb782fb94fad
Ezuker <benoit.carolle.pro@gmail.com>
2024-12-11 19:48:20 +0100

    feat: adding calcul delta to get the solution. fix: few fix in the parse

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import sys as sys
 from re import findall
+from math import sqrt
 
 
 def format_term(coef, power, term=True):
 	if coef == 0:
 		return ""
-	if coef < 0:
+	if coef < 0 and term == True:
 		return f"- {-coef} * X^{power} "
 	if term == True:
 		return f"+ {coef} * X^{power} "
 	return f"{coef} * X^{power} "
+
 
 def printer(left: "PolynomialEquationSolver", right: "PolynomialEquationSolver"):
 	# Collect non-zero terms for left and right sides
 	left_terms = [
 		format_term(left.c, 0, term=False),
 		format_term(left.b, 1, term=left.c != 0),
-		format_term(left.a, 2, term=left.b != 0)
+		format_term(left.a, 2, term=left.b != 0 or left.c != 0)
 	]
 	# left_terms = [term for term in left_terms if term]
 
 	right_terms = [
 		format_term(right.c, 0, term=False),
 		format_term(right.b, 1, term=right.c != 0),
-		format_term(right.a, 2, term=right.b != 0)
+		format_term(right.a, 2, term=right.b != 0 or right.c != 0)
 	]
 	# right_terms = [term for term in right_terms if term]
 
@@ -49,11 +51,19 @@ class PolynomialEquationSolver:
 
 	def parse(self):
 		self.terms = findall(r'([+-]?\s*\d*\.?\d*)\s*\*?\s*X\^?(\d*)', self.equation)
-		self.c = float(self.terms[0][0].replace(" ", ""))
-		self.b = float(self.terms[1][0].replace(" ", ""))
-		self.a = float(self.terms[2][0].replace(" ", ""))
-
-
+		self.c = 0
+		self.b = 0
+		self.a = 0
+		for term in self.terms:
+			if term[1] == "0":
+				self.c = float(term[0].replace(" ", ""))
+			elif term[1] == "1":
+				self.b = float(term[0].replace(" ", ""))
+			elif term[1] == "2":
+				self.a = float(term[0].replace(" ", ""))
+			else:
+				raise ValueError("c bon")
+		
 
 	def reductionForm(self, otherSide: "PolynomialEquationSolver"):
 		printer(self, otherSide)
@@ -67,6 +77,22 @@ class PolynomialEquationSolver:
 		otherSide.a = 0
 		printer(self, otherSide)
 
+	
+	def calculDelta(self):
+		delta = self.b ** 2 - 4 * self.a * self.c
+		if delta < 0:
+			print("The discriminant is negative")
+			print("The equation doesn't have reel solutions")
+		elif delta == 0:
+			print("The discriminant is equal to 0")
+			print("There is one solution:")
+			print(f"x : {-self.b / (self.a * 2)}")
+		else:
+			print("The discriminant is positive")
+			print("There is two solutions:")
+			print(f"x1 : {(-self.b + sqrt(delta)) / (2 * self.a)}")
+			print(f"x2 : {(-self.b - sqrt(delta)) / (2 * self.a)}")
+
 
 
 def main():
@@ -77,6 +103,7 @@ def main():
 		left.parse()
 		right.parse()
 		left.reductionForm(right)
+		left.calculDelta()
 	# except Exception as e:
 	# 	print(e)
 
