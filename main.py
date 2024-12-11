97643c7d76009672feb734da2a35e5f306621395
Ezuker <benoit.carolle.pro@gmail.com>
2024-12-11 21:10:56 +0100

    refactor + fix: handle parsing of N degree, clean the code by using loop instead of a b c

--- main.py
+++ main.py
@@ -14,22 +14,22 @@ def format_term(coef, power, term=True):
 
 
 def printer(left: "PolynomialEquationSolver", right: "PolynomialEquationSolver"):
-	# Collect non-zero terms for left and right sides
-	left_terms = [
-		format_term(left.c, 0, term=False),
-		format_term(left.b, 1, term=left.c != 0),
-		format_term(left.a, 2, term=left.b != 0 or left.c != 0)
-	]
-	# left_terms = [term for term in left_terms if term]
+	left_terms = []
+	term = False
+	for power, coef in left.coef.items():
+		str = format_term(coef, int(power), term)
+		if str:
+			left_terms.append(str)
+			term = True
 
-	right_terms = [
-		format_term(right.c, 0, term=False),
-		format_term(right.b, 1, term=right.c != 0),
-		format_term(right.a, 2, term=right.b != 0 or right.c != 0)
-	]
-	# right_terms = [term for term in right_terms if term]
+	right_terms = []
+	term = False
+	for power, coef in right.coef.items():
+		str = format_term(coef, int(power), term)
+		if str:
+			right_terms.append(str)
+			term = True
 
-	# Join the terms
 	left_equation = ''.join(left_terms).strip()
 	right_equation = ''.join(right_terms).strip()
 
@@ -51,52 +51,56 @@ class PolynomialEquationSolver:
 
 	def parse(self):
 		self.terms = findall(r'([+-]?\s*\d*\.?\d*)\s*\*?\s*X\^?(\d*)', self.equation)
-		self.c = 0
-		self.b = 0
-		self.a = 0
+		self.coef = {}
 		for term in self.terms:
-			if term[1] == "0":
-				self.c = float(term[0].replace(" ", ""))
-			elif term[1] == "1":
-				self.b = float(term[0].replace(" ", ""))
-			elif term[1] == "2":
-				self.a = float(term[0].replace(" ", ""))
-			else:
-				raise ValueError("c bon")
+			self.coef[term[1].replace(" ", "")] = float(term[0].replace(" ", ""))
 		
 
 	def reductionForm(self, otherSide: "PolynomialEquationSolver"):
+		nb_step = 1
+		print(f"Reduce form step {nb_step}")
 		printer(self, otherSide)
-		self.c = self.c - otherSide.c
-		otherSide.c = 0
-		printer(self, otherSide)
-		self.b = self.b - otherSide.b
-		otherSide.b = 0
-		printer(self, otherSide)
-		self.a = self.a - otherSide.a
-		otherSide.a = 0
-		printer(self, otherSide)
+		for power, coef in self.coef.items():
+			other_coef = otherSide.coef.get(power, 0)
+			self.coef[power] = coef - other_coef
+			if power in otherSide.coef:
+				otherSide.coef[power] = 0
+				nb_step = nb_step + 1
+				print(f"Reduce form step {nb_step}")
+				printer(self, otherSide)
 
 	
 	def calculDelta(self):
-		delta = self.b ** 2 - 4 * self.a * self.c
-		if delta < 0:
-			print("The discriminant is negative")
-			print("The equation doesn't have reel solutions")
-		elif delta == 0:
-			print("The discriminant is equal to 0")
-			print("There is one solution:")
-			print(f"x : {-self.b / (self.a * 2)}")
+		last_item = int(list(self.coef.items())[-1][0])
+		if last_item > 2:
+			print(f"Polynomial degree: {last_item}")
+			print(f"The polynomial degree is strictly greater than 2, I can't solve.")
+			return
+		if last_item == 2:
+			print("Polynomial degree: 2")
+			delta = self.coef['1'] ** 2 - 4 * self.coef['2'] * self.coef['0']
+			if delta < 0:
+				print("The discriminant is negative")
+				print("The equation doesn't have reel solutions")
+			elif delta == 0:
+				print("The discriminant is equal to 0")
+				print("There is one solution:")
+				print(f"x : {-self.coef['1'] / (self.coef['2'] * 2)}")
+			else:
+				print("The discriminant is positive")
+				print("There is two solutions:")
+				print(f"x1 : {(-self.coef['1'] + sqrt(delta)) / (2 * self.coef['2'])}")
+				print(f"x2 : {(-self.coef['1'] - sqrt(delta)) / (2 * self.coef['2'])}")
+		elif last_item == 1:
+			print("Polynomial degree: 1")
+			print("The solution is :")
+			print(f"{self.coef['0'] / -self.coef['1']}")
 		else:
-			print("The discriminant is positive")
-			print("There is two solutions:")
-			print(f"x1 : {(-self.b + sqrt(delta)) / (2 * self.a)}")
-			print(f"x2 : {(-self.b - sqrt(delta)) / (2 * self.a)}")
-
+			print("Please provide a polynomial equation")
 
 
 def main():
-	# try:
+	try:
 		left, right = sys.argv[1].split("=")
 		left = PolynomialEquationSolver(left.strip())
 		right = PolynomialEquationSolver(right.strip())
@@ -104,8 +108,8 @@ def main():
 		right.parse()
 		left.reductionForm(right)
 		left.calculDelta()
-	# except Exception as e:
-	# 	print(e)
+	except Exception as e:
+		print(e)
 
 
 if __name__ == '__main__':
