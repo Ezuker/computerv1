a5b93d1a350d9913c1bade1938900a526b41aaac
Ezuker <benoit.carolle.pro@gmail.com>
2024-12-12 21:06:21 +0100

    fix: adjust x variable for the graph

--- graphic.py
+++ graphic.py
@@ -11,30 +11,21 @@ def translateReducedForm(reducedForm, x):
 
 def graphicDisplay(reducedForm, result):
 	if len(result) == 2:
-		x = np.linspace(result[0] - (result[0] / 10), result[1] + (result[1] / 10), 400)
+		x = np.linspace(result[0] + (result[0] / 5), result[1] + (result[1] / 5), 400)
 	elif len(result) == 1:
-		x = np.linspace(result[0] - (result[0] / 10), result[0] + (result[0] / 10), 400)  # 400 points between -10 and 10
+		x = np.linspace(result[0] - (result[0] / 5), result[0] + (result[0] / 5), 400)
 	else:
 		return
 
-	# Calculate the corresponding y values
 	y = translateReducedForm(reducedForm, x)
 
-	# Create the plot
 	plt.plot(x, y, label=f'{printer(reducedForm, {"0": 0.0})}')
-
 	for point in result:
 		plt.scatter(point, translateReducedForm(reducedForm, point), color='red')
 
-
-	# Add labels and title
 	plt.xlabel('x')
 	plt.ylabel('y')
 	plt.title(f'Plot of the quadratic function {printer(reducedForm, {"0": 0.0})}')
-
-	# Add a legend
 	plt.legend()
-
-	# Show the plot
 	plt.grid(True)
 	plt.show()
